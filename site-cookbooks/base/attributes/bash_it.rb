af5595da85e5507d2e59b2338c6265633eedc41e
Ian Lesperance <ian@elliterate.com>
2017-01-26 09:35:22 -0500

    Use custom shell aliases and colors

--- site-cookbooks/base/attributes/bash_it.rb
+++ site-cookbooks/base/attributes/bash_it.rb
@@ -5,6 +5,12 @@ node.default['bash_it'] = {
   'enabled_plugins' => {
     'plugins' => %w(base)
   },
+  'custom_plugins' => {
+    'base' => %w(
+      bash_it/custom/aliases.bash
+      bash_it/custom/colors.bash
+    )
+  },
   'theme' => 'powerline',
   'dir' => ::File.expand_path('.bash_it', node['workstation']['home']),
   'bashrc_path' => ::File.expand_path('.bash_profile', node['workstation']['home']),
---
+++ site-cookbooks/base/templates/default/bash_it/custom/colors.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export LSCOLORS="ExGxFxDxCxEGEDABADACEG"
+export GREP_OPTIONS="--color=auto"
+export GREP_COLOR="1;36"
