6281508a4670e8e56d00ecc2bf859569f53f3c22
Ian Lesperance <ian@elliterate.com>
2017-01-29 09:37:41 -0500

    Use function keys as function keys

--- site-cookbooks/osx-settings/recipes/default.rb
+++ site-cookbooks/osx-settings/recipes/default.rb
@@ -1,4 +1,5 @@
 include_recipe 'osx-settings::set_key_repeat_preferences'
+include_recipe 'osx-settings::function_keys'
 include_recipe 'osx-settings::inputrc'
 include_recipe 'osx-settings::keyboard'
 include_recipe 'osx-settings::locate'
