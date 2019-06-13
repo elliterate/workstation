af36fa788df8508bdab91833d27e55391a12f8a8
Ian Lesperance <ian@elliterate.com>
2019-06-13 10:52:38 -0400

    Enable git completions

---
+++ site-cookbooks/git/recipes/completions.rb
@@ -0,0 +1,3 @@
+include_recipe 'base::bash_it'
+
+base_bash_it_enable_feature 'completion/git'
--- site-cookbooks/git/recipes/default.rb
+++ site-cookbooks/git/recipes/default.rb
@@ -1,3 +1,4 @@
 include_recipe 'git::install'
 include_recipe 'git::global_config'
 include_recipe 'git::global_ignore'
+include_recipe 'git::completions'
