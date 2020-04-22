8dbf606eaf4e0c8f00fa3ff059171148f3e29f16
Ian Lesperance <ian@elliterate.com>
2020-04-22 12:25:13 -0400

    Store psql history per user and database

--- site-cookbooks/postgresql/recipes/psqlrc.rb
+++ site-cookbooks/postgresql/recipes/psqlrc.rb
@@ -2,3 +2,9 @@ template "#{node['workstation']['home']}/.psqlrc" do
   owner node['workstation']['user']
   source 'psqlrc.erb'
 end
+
+directory "#{node['workstation']['home']}/.psql" do
+  owner node['workstation']['user']
+  mode '0755'
+  action :create
+end
