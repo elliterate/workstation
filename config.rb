f0d28dc0b568f888d98f80f3df1ef1e301c2e06a
Ian Lesperance <ian@elliterate.com>
2020-04-23 12:04:03 -0400

    Add ohai plugin to load /etc/passwd data
    This functionality was made optional.

--- config.rb
+++ config.rb
@@ -3,3 +3,6 @@ root = File.absolute_path(File.dirname(__FILE__))
 file_cache_path File.join(ENV['HOME'], '.cache', 'chef')
 cookbook_path File.join(root, 'cookbooks')
 json_attribs File.join(root, 'node.json')
+ohai.optional_plugins = [
+  :Passwd,
+]
