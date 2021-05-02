623e8ecf6f6513497032c5ac1847b00bd9de526e
Ian Lesperance <ian@elliterate.com>
2021-05-02 11:26:34 -0400

    Disable text replacement in new TextEdit documents

--- site-cookbooks/osx-settings/recipes/textedit_preferences.rb
+++ site-cookbooks/osx-settings/recipes/textedit_preferences.rb
@@ -3,3 +3,9 @@ osx_defaults 'disable rich text in new TextEdit documents' do
   key 'RichText'
   boolean false
 end
+
+osx_defaults 'disable text replacement in new TextEdit documents' do
+  domain 'com.apple.TextEdit'
+  key 'TextReplacement'
+  boolean false
+end
