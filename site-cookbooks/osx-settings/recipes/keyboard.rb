86d8fb3d9e7739760333e1bfc67c0b434cf89dcf
Ian Lesperance <ian@elliterate.com>
2023-02-02 14:49:46 -0500

    Disable "Search man Page" keyboard shortcut
    This conflicts with the "Find Action..." keyboard shortcut in JetBrains
    IDEs.

--- site-cookbooks/osx-settings/recipes/keyboard.rb
+++ site-cookbooks/osx-settings/recipes/keyboard.rb
@@ -1,6 +1,36 @@
+require 'shellwords'
+
 osx_defaults 'enable full keyboard access for all controls' do
   # from http://knoopx.net/os-x-lion-tweaks
   domain 'NSGlobalDomain'
   key 'AppleKeyboardUIMode'
   integer 3
+end
+
+disabled_keyboard_shortcut_xml =
+  <<~XML
+    <dict>
+      <key>enabled_context_menu</key>
+      <false/>
+      <key>enabled_services_menu</key>
+      <false/>
+      <key>presentation_modes</key>
+      <dict>
+        <key>ContextMenu</key>
+        <false/>
+        <key>ServicesMenu</key>
+        <false/>
+      </dict>
+    </dict>
+  XML
+
+disabled_service_keyboard_shortcut_keys = [
+  'com.apple.Terminal - Search man Page Index in Terminal - searchManPages',
+]
+
+disabled_service_keyboard_shortcut_keys.each do |key|
+  execute "disable #{key.inspect} service keyboard shortcut" do
+    command "defaults write pbs NSServicesStatus -dict-add #{Shellwords.escape(key)} #{Shellwords.escape(disabled_keyboard_shortcut_xml)}"
+    user node['workstation']['user']
+  end
 end
